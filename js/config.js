1e4ba0579ee3c16867a1a7bd9d0351a3c9c116d9
Mark Lockett <mark@csm.cc>
2020-06-24 14:59:40 +0000

    geesh, just follow cognito instructions

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_NwLfw1NYK', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '61t1h1pk5ootvahd2njhologe0', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_lQXdc4us8', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4na3o94gpgcm9m25epa4lbvahd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
